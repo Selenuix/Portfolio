ffea3f436c6a04b0b1881ad6b4d9b5df0f2ecb4c
Selenuix <anthony.ciszek@outlook.com>
2024-01-27 22:06:40 +0100

    chore: initial commit

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Anthony Ciszek | Développeur Web Fullstack';
+export const SITE_DESCRIPTION = 'Découvrez le parcours d\'Anthony Ciszek, un développeur web passionné. Plongez dans une vitrine de projets innovants, de conceptions étonnantes et d\'expériences numériques percutantes.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
